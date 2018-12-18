db35ab9ab90d706eb6395b4df12361831f458066
hiroshi sasmita <hiroshisasmita@gmail.com>
2018-12-18 13:39:38 +0700

    sudah lama, fix dinamic pic

---
+++ app/src/main/java/com/project/pentacode/pomestaff/CobaCoba.java
@@ -0,0 +1,13 @@
+package com.project.pentacode.pomestaff;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CobaCoba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coba_coba);
+    }
+}
---
+++ app/src/main/java/com/project/pentacode/pomestaff/model/LoginUser.java
@@ -0,0 +1,4 @@
+package com.project.pentacode.pomestaff.model;
+
+public class LoginUser {
+}
